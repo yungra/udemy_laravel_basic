c8a1d7ebfdb8eae7fd1748555070e28d545e869d
yungra <yhuga01@gmail.com>
2022-10-08 18:12:10 +0900

    first

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB; 
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        dd('test');
+
+        // Eloquent(エロクアント)
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb');
+
+        //クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
+
+        // dd($values);
+
+        return view('tests.test', compact('values'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
+use App\Http\Controllers\ShopController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('tests/test', [ TestController::class, 'index']);
+
+Route::get('shops', [ ShopController::class, 'index']);
+
+// Route::resource('contacts', ContactFormController::class);
+
+
+Route::prefix('contacts')->middleware(['auth'])
+->controller(ContactFormController::class)
+->name('contacts.')
+->group(function() {
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{id}', 'show')->name('show');
+    Route::get('/{id}/edit', 'edit')->name('edit');
+    Route::post('/{id}', 'update')->name('update');
+    Route::get('/{id}/destroy', 'destroy')->name('destroy');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
